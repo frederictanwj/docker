b23d7f96711617d66c85341dcb0d078b3af8bacd
Frederic Tan <frederictan@fred-mac.local>
2022-01-11 16:24:27 -0500

    update

--- kafka/kafka_client.sh
+++ kafka/kafka_client.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name kafka_cli --network kafka_nwk -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper_svr:2181 bitnami/kafka:3 bash
+docker run -it --rm --network kafka_net bitnami/kafka:3 bash
--- kafka/kafka_server.sh
+++ kafka/kafka_server.sh
@@ -1,3 +1,14 @@
-docker run -d --name zookeeper_svr --network kafka_nwk -v zookeeper_vol:/bitnami/zookeeper -e ALLOW_ANONYMOUS_LOGIN=yes bitnami/zookeeper:3.7
+# docker network create kafka_net
+# docker volume create zookeeper_vol
+# docker volume create kafka_vol
 
-docker run -d --name kafka_svr --network kafka_nwk -v kafka_vol:/bitnami/kafka -e ALLOW_PLAINTEXT_LISTENER=yes -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper_svr:2181 bitnami/kafka:3 
+# zookeeper
+# docker run -d --name zookeeper_svr --network kafka_net -v zookeeper_vol:/bitnami/zookeeper -e ALLOW_ANONYMOUS_LOGIN=yes bitnami/zookeeper:3.7
+docker run -d --name zookeeper_svr --network kafka_net -e ZOOKEEPER_CLIENT_PORT=2181 confluentinc/cp-zookeeper:latest
+
+# kafka broker 1
+# docker run -d --name kafka_svr1 --network kafka_net -p 9092:9092 -v kafka_vol:/bitnami/kafka -e ALLOW_PLAINTEXT_LISTENER=yes -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper_svr:2181 bitnami/kafka:3
+docker run -d --name kafka_svr --network kafka_net -p 9092:9092 -e KAFKA_ZOOKEEPER_CONNECT=zookeeper_svr:2181 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:9092 -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 confluentinc/cp-kafka:latest
+
+# kafka broker 2
+# docker run -d --name kafka_svr2 --network kafka_net -e ALLOW_PLAINTEXT_LISTENER=yes -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper_svr:2181 bitnami/kafka:3
